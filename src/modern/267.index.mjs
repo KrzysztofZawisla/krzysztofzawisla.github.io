48b337a41a73fdb8e58467673b7b66100c92a82e
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-06 21:03:31 +0100

    Updates

---
+++ src/modern/267.index.mjs
@@ -0,0 +1,18 @@
+(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[267],{3614:(e,s,r)=>{"use strict";r.d(s,{Z:()=>t});const t=r(2993).ZP.article`
+  background: #eee;
+  padding: 7.5px;
+  border-radius: 7.5px;
+  margin-bottom: 15px;
+`},6037:(e,s,r)=>{"use strict";r.d(s,{Z:()=>a});var t=r(7557),n=r(9526),i=r(1011),l=r(187);const c=r(2993).ZP.div`
+  width: 80%;
+  margin: 0 auto;
+  word-break: break-all;
+  @media screen and (min-width: 768px) {
+    & {
+      word-break: normal;
+    }
+  }
+`,a=({title:e,children:s})=>{const{languageDispatcher:r}=(0,n.useContext)(l.ZP),[a]=r,d="Krzysztof Zawisła"+(""!==e?` - ${e}`:"");return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsxs)(i.q,{htmlAttributes:{lang:a},children:[(0,t.jsx)("title",{children:d}),(0,t.jsx)("meta",{name:"og:title",content:d})]}),(0,t.jsx)(c,{children:s})]})}},4266:(e,s,r)=>{"use strict";r.d(s,{Z:()=>t});const t=r(2993).ZP.section`
+  text-align: justify;
+`},8267:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>a});var t=r(7557),n=r(6037),i=r(3614),l=r(4266),c=r(5311);const a=()=>{const{t:e}=(0,c.$)(),s=e("pages.experience.vacations"),r=e("pages.experience.heading");return(0,t.jsxs)(n.Z,{title:r,children:[(0,t.jsxs)("h2",{children:[r,":"]}),(0,t.jsxs)(i.Z,{children:[(0,t.jsx)("h3",{children:"Front-end Developer"}),(0,t.jsxs)(l.Z,{children:["Euvic - ",s," 2020"]})]}),(0,t.jsxs)(i.Z,{children:[(0,t.jsx)("h3",{children:"Front-end Developer"}),(0,t.jsxs)(l.Z,{children:["Ephp - ",e("pages.experience.schoolPractice")]})]}),(0,t.jsxs)(i.Z,{children:[(0,t.jsx)("h3",{children:"Full-Stack Developer"}),(0,t.jsxs)(l.Z,{children:["COIG S.A. - ",s," 2019"]})]}),(0,t.jsx)(i.Z,{children:(0,t.jsx)("a",{href:"https://bulldogjob.pl/blog/authors/krzysztof-zawisla",title:"BulldogJob",children:(0,t.jsx)("h3",{children:e("pages.experience.bulldogjob")})})})]})}}}]);
+//# sourceMappingURL=267.index.mjs.map
---
+++ src/modern/960.index.mjs
@@ -0,0 +1,22 @@
+(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[960],{3614:(t,e,s)=>{"use strict";s.d(e,{Z:()=>r});const r=s(2993).ZP.article`
+  background: #eee;
+  padding: 7.5px;
+  border-radius: 7.5px;
+  margin-bottom: 15px;
+`},6037:(t,e,s)=>{"use strict";s.d(e,{Z:()=>d});var r=s(7557),i=s(9526),a=s(1011),n=s(187);const o=s(2993).ZP.div`
+  width: 80%;
+  margin: 0 auto;
+  word-break: break-all;
+  @media screen and (min-width: 768px) {
+    & {
+      word-break: normal;
+    }
+  }
+`,d=({title:t,children:e})=>{const{languageDispatcher:s}=(0,i.useContext)(n.ZP),[d]=s,c="Krzysztof Zawisła"+(""!==t?` - ${t}`:"");return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsxs)(a.q,{htmlAttributes:{lang:d},children:[(0,r.jsx)("title",{children:c}),(0,r.jsx)("meta",{name:"og:title",content:c})]}),(0,r.jsx)(o,{children:e})]})}},4266:(t,e,s)=>{"use strict";s.d(e,{Z:()=>r});const r=s(2993).ZP.section`
+  text-align: justify;
+`},7960:(t,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>c});var r=s(7557),i=s(6037);const a=s(2993).ZP.img`
+  width: 100px;
+  height: auto;
+  border-radius: 7.5px;
+`;var n=s(3614),o=s(4266),d=s(5311);const c=()=>{const{t}=(0,d.$)(),e=t("pages.aboutMe.heading");return(0,r.jsxs)(i.Z,{title:e,children:[(0,r.jsxs)("h2",{children:[e,":"]}),(0,r.jsxs)(n.Z,{children:[(0,r.jsx)("h3",{children:"Who am I?"}),(0,r.jsx)(a,{title:"Krzysztof Zawisła",alt:"Krzysztof Zawisła",src:"https://avatars0.githubusercontent.com/u/32432944?s=460&v=4",loading:"lazy",height:"100px",width:"100px"}),(0,r.jsx)(o.Z,{children:"123"})]})]})}}}]);
+//# sourceMappingURL=960.index.mjs.map
