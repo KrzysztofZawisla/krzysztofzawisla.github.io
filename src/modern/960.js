a5ceb6ea8b62f1f2dad6a2989eb114b9eb9f394a
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-09 01:07:43 +0100

    Updates

--- src/modern/960.js
+++ src/modern/960.js
@@ -3,15 +3,15 @@
   padding: 7.5px;
   border-radius: 7.5px;
   margin-bottom: 15px;
-`},6037:(t,s,e)=>{"use strict";e.d(s,{Z:()=>c});var r=e(7557),i=e(9526),a=e(1011),n=e(187);const o=e(2993).ZP.div`
+`},6037:(t,s,e)=>{"use strict";e.d(s,{Z:()=>c});var r=e(9526),i=e(1011),a=e(187);const n=e(2993).ZP.div`
   width: 80%;
   margin: 0 auto;
   word-break: break-word;
-`,c=({title:t,children:s})=>{const{languageDispatcher:e}=(0,i.useContext)(n.ZP),[c]=e,d="Krzysztof Zawisła"+(""!==t?` - ${t}`:"");return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsxs)(a.q,{htmlAttributes:{lang:c},children:[(0,r.jsx)("title",{children:d}),(0,r.jsx)("meta",{name:"og:title",content:d})]}),(0,r.jsx)(o,{children:s})]})}},4266:(t,s,e)=>{"use strict";e.d(s,{Z:()=>r});const r=e(2993).ZP.section`
+`;var o=e(7557);const c=({title:t,children:s})=>{const{languageDispatcher:e}=(0,r.useContext)(a.ZP),[c]=e,d="Krzysztof Zawisła"+(""!==t?` - ${t}`:"");return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsxs)(i.q,{htmlAttributes:{lang:c},children:[(0,o.jsx)("title",{children:d}),(0,o.jsx)("meta",{name:"og:title",content:d})]}),(0,o.jsx)(n,{children:s})]})}},4266:(t,s,e)=>{"use strict";e.d(s,{Z:()=>r});const r=e(2993).ZP.section`
   text-align: justify;
-`},7960:(t,s,e)=>{"use strict";e.r(s),e.d(s,{default:()=>d});var r=e(7557),i=e(6037);const a=e(2993).ZP.img`
+`},7960:(t,s,e)=>{"use strict";e.r(s),e.d(s,{default:()=>d});var r=e(6037);const i=e(2993).ZP.img`
   width: 100px;
   height: auto;
   border-radius: 7.5px;
-`;var n=e(3614),o=e(4266),c=e(5311);const d=()=>{const{t}=(0,c.$)(),s=t("pages.aboutMe.heading");return(0,r.jsxs)(i.Z,{title:s,children:[(0,r.jsxs)("h2",{children:[s,":"]}),(0,r.jsxs)(n.Z,{children:[(0,r.jsx)("h3",{children:"Who am I?"}),(0,r.jsx)(a,{title:"Krzysztof Zawisła",alt:"Krzysztof Zawisła",src:"https://avatars0.githubusercontent.com/u/32432944?s=460&v=4",loading:"lazy",height:"100px",width:"100px"}),(0,r.jsx)(o.Z,{children:"123"})]})]})}}}]);
+`;var a=e(3614),n=e(4266),o=e(5311),c=e(7557);const d=()=>{const{t}=(0,o.$)(),s=t("pages.aboutMe.heading");return(0,c.jsxs)(r.Z,{title:s,children:[(0,c.jsxs)("h2",{children:[s,":"]}),(0,c.jsxs)(a.Z,{children:[(0,c.jsx)("h3",{children:"Who am I?"}),(0,c.jsx)(i,{title:"Krzysztof Zawisła",alt:"Krzysztof Zawisła",src:"https://avatars0.githubusercontent.com/u/32432944?s=460&v=4",loading:"lazy",height:"100px",width:"100px"}),(0,c.jsx)(n.Z,{children:"123"})]})]})}}}]);
 //# sourceMappingURL=960.js.map
