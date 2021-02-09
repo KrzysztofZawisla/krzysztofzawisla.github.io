2c6fb72ccd369d88d2d0bbfea01b84b2145e8670
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-10 00:45:09 +0100

    Fixed tests

--- src/components/Bold/Bold.test.tsx
+++ src/components/Bold/Bold.test.tsx
@@ -1,6 +1,23 @@
 import Bold from "./Bold";
-import { render } from "@testing-library/react";
+import { render, cleanup, screen, RenderResult } from "@testing-library/react";
+import { ReactElement } from "react";
 
-test("Render Bold component", async (): Promise<void> => {
-  render(<Bold>test</Bold>);
+afterEach(cleanup);
+
+const testText: string = "test";
+const TestComponent: ReactElement = <Bold>{testText}</Bold>;
+
+describe("Bold", (): void => {
+  it("renders component properly", async (): Promise<void> => {
+    const { container }: RenderResult = render(TestComponent);
+    expect(container).toMatchSnapshot();
+    expect(await screen.findByText(testText)).toBeInTheDocument();
+  });
+  it("is bold", async (): Promise<void> => {
+    render(TestComponent);
+    const element: HTMLSpanElement = screen.getByText(
+      testText,
+    ) as HTMLSpanElement;
+    expect(element).toHaveStyleRule("font-weight", "700");
+  });
 });
