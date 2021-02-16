ce3bd2838a8dae139db0632a50d3da627e40fb98
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-16 16:30:11 +0100

    Added tsd

--- src/other/translation.test.ts
+++ src/other/translation.test.ts
@@ -1,9 +1,11 @@
 import { i18n } from "i18next";
 import initTranslations from "./translation";
+import { expectType } from "tsd";
 
 describe("Translation", () => {
   it("Returns value", async () => {
     const translation: i18n = await initTranslations();
+    expectType<i18n>(translation);
     expect(translation).toBeTruthy();
   });
 });
