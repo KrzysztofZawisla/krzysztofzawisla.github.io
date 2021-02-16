f93642f74203cf624a5e71e2007f7ff1721140b6
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-16 19:44:51 +0100

    Fixed test

--- src/other/translation/translation.test.ts
+++ src/other/translation/translation.test.ts
@@ -1,11 +1,15 @@
 import { i18n } from "i18next";
 import initTranslations from "./translation";
 import { expectType } from "tsd";
+import { isEmpty } from "lodash";
 
 describe("Translation", () => {
   it("Returns value", async () => {
     const translation: i18n = await initTranslations();
-    expectType<i18n>(translation);
+    expect(isEmpty(translation)).toBe(false);
     expect(translation).toBeTruthy();
+  });
+  it("Has correct type", async () => {
+    expectType<i18n>(await initTranslations());
   });
 });
