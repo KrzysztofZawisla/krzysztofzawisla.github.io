5d781591b612812205fff52987f8da2287abdffc
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-22 13:15:27 +0100

    Added no-console

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,11 @@ import initTranslations from "./other/translation/translation";
 
 if (process.env.DEVELOPMENT) {
   const { getLCP, getFID, getCLS } = await import("web-vitals");
+  // eslint-disable-next-line no-console
   getLCP(console.log);
+  // eslint-disable-next-line no-console
   getFID(console.log);
+  // eslint-disable-next-line no-console
   getCLS(console.log);
 }
 
--- src/other/consoleGreetings/consoleGreetings.ts
+++ src/other/consoleGreetings/consoleGreetings.ts
@@ -1,4 +1,5 @@
 const consoleGreetings = (): void => {
+  // eslint-disable-next-line no-console
   console.log(
     "%c+",
     "font-size: 1px; padding: 80px 310px; line-height: 180px; background: url(https://i.gifer.com/Um3p.webp); background-size: 640px 360px; color: transparent;",
