3665caf21ddde948be524cf5f2294f4f65ad91cc
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-24 14:33:51 +0100

    Added error text

--- src/pages/ErrorLoading.tsx
+++ src/pages/ErrorLoading.tsx
@@ -2,14 +2,22 @@ import { FC } from "react";
 import Page from "../components/Page";
 import Article from "../components/Article/Article";
 import Section from "../components/Section/Section";
+import { useTranslation } from "react-i18next";
 
 const ErrorLoading: FC = (): JSX.Element => {
-  const heading: string = "Błąd ładowania";
+  const { t } = useTranslation();
+  const heading: string = t("pages.errorLoading.heading");
   return (
     <Page title={heading}>
       <h2>{heading}:</h2>
       <Article>
-        <Section>Wystąpił błąd ładowania strony.</Section>
+        <Section>
+          {`${t("pages.errorLoading.error")} `}
+          <span role="img" aria-label="crying emoji">
+            😢
+          </span>
+          .
+        </Section>
       </Article>
     </Page>
   );
