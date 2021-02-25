10cc850e07f583ad999e2626a5e7c92275673258
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-25 08:43:58 +0100

    Fixed style

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -6,6 +6,9 @@ const Article = memo(styled.article`
   padding: 7.5px;
   border-radius: 7.5px;
   margin-bottom: 15px;
+  &:nth-last-child(1) {
+    margin-bottom: 0;
+  }
 `);
 
 export default Article;
