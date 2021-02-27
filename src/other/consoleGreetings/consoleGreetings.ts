d4bca76b8f0bff0ef298dbc48d2c76e68947eafe
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-02-27 20:53:06 +0100

    Typed code

--- src/other/consoleGreetings/consoleGreetings.ts
+++ src/other/consoleGreetings/consoleGreetings.ts
@@ -1,4 +1,4 @@
-export const consoleGreetingsArgument = [
+export const consoleGreetingsArgument: string[] = [
   "%c+",
   "font-size: 1px; padding: 80px 310px; line-height: 180px; background: url(https://i.gifer.com/Um3p.webp); background-size: 640px 360px; color: transparent;",
 ];
