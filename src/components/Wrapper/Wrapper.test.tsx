8ac1c63d4e568135c04362d5c50acca2df9617a1
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2021-03-01 14:49:13 +0100

    Typed code and added test

--- src/components/Wrapper/Wrapper.test.tsx
+++ src/components/Wrapper/Wrapper.test.tsx
@@ -1,5 +1,6 @@
-import Wrapper from "./Wrapper";
+import Wrapper, { WrapperType } from "./Wrapper";
 import { cleanup, render, RenderResult } from "@testing-library/react";
+import { expectType } from "tsd";
 
 afterEach(cleanup);
 
@@ -7,5 +8,8 @@ describe("Wrapper", (): void => {
   it("renders component properly", async (): Promise<void> => {
     const { container }: RenderResult = render(<Wrapper>test</Wrapper>);
     expect(container).toMatchSnapshot();
+  });
+  it("Has correct type", () => {
+    expectType<WrapperType>(Wrapper);
   });
 });
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,7 +1,12 @@
-import { memo } from "react";
-import styled from "styled-components";
+import { memo, MemoExoticComponent } from "react";
+import styled, { StyledComponent } from "styled-components";
 
-const Wrapper = memo(styled.div`
+export type WrapperType = MemoExoticComponent<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  StyledComponent<"div", never, {}, never>
+>;
+
+const Wrapper: WrapperType = memo(styled.div`
   width: 80%;
   margin: 0 auto;
   word-break: break-word;
