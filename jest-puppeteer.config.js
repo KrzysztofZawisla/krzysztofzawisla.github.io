a0e8e10e509b22e82f1282fbc4cc64a4ec9291ea
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2021-06-27 20:03:04 +0200

    Changed tsconfig

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  launch: {
+    dumpio: true,
+    headless: true,
+    args: ["--disable-infobars"],
+  },
+  browserContext: "default",
+};
