3a8dce0acadc9d4582138d2c19fb42b3e6b828a8
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2021-07-06 21:49:34 +0200

    Added ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.krzysztofzawislaportfolio",
+  appName: "Krzysztof Zawisła - Portfolio",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
